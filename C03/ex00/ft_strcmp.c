b388da03b5ddebda5b0675fee18fba0233c545e7
andy <andy.ka@hotmail.fr>
2023-03-13 23:23:10 +0100

    c03 00

---
+++ C03/ex00/ft_strcmp.c
@@ -0,0 +1,18 @@
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && (s1[i] == s2[i]))
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+#include <stdio.h>
+#include <string.h>
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%d\n%d", ft_strcmp(argv[1], argv[2]), strcmp(argv[3], argv[4]));
+}
